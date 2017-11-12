77af3509e5fdb538190881f833c1f6dbda60bdef
Tiffany Quon <tiffany.quon@alumni.ubc.ca>
2017-11-11 17:27:42 -0800

    testing

--- App.js
+++ App.js
@@ -69,7 +69,7 @@ render() {
       <View style={{paddingTop:30, alignItems: 'center'}}>
         <TouchableHighlight onPress={() => navigate('Home')} underlayColor="white">
               <View style={styles.button}>
-                <Text style={styles.buttonText}>Sign in</Text>
+                <Text style={styles.buttonText}>Sign In</Text>
               </View>
           </TouchableHighlight>
           <Text style={{paddingTop:2}}></Text>
@@ -87,8 +87,12 @@ render() {
 }
 
 class HomeScreen extends React.Component {
+   const { navigate } = this.props.navigation;
+  
   static navigationOptions = {
   	title: 'Welcome Back!',
+    headerLeft: <Button onPress={() => navigate('Welcome')}
+    title="Add Friend"/>,
     headerRight: <Button onPress={() => { Alert.alert('You tapped the button!')}}
     title="Add Friend"/>
   };
@@ -119,9 +123,9 @@ class AddFriends extends React.Component {
 
 const TabsApp = StackNavigator({
   Welcome: 	{screen: WelcomeScreen},
-  Login: 	{screen: LoginScreen},
+  Login: 	  {screen: LoginScreen},
   Home:   	{screen: HomeScreen},
-  Add: 		{screen: AddFriends}},
+  Add: 	  	{screen: AddFriends}},
 
   {
   	cardStyle: {backgroundColor: 'white'}
